87a8b30ebab9f336a5c4333da73820a5c8c6feb3
Senthilkumar <psysenthilkumar@gmail.com>
2021-02-10 13:59:51 +0530

    log changes

--- src/main/java/com/test/ScheduledLog.java
+++ src/main/java/com/test/ScheduledLog.java
@@ -2,8 +2,8 @@ package com.test;
 
 import java.time.LocalDateTime;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -11,13 +11,14 @@ import org.springframework.stereotype.Component;
 @Component
 @EnableScheduling
 public class ScheduledLog {
-	private static final Logger LOGGER = LoggerFactory.getLogger(ScheduledLog.class);
+	private static final Logger logger = LogManager.getLogger(ScheduledLog.class.getSimpleName());
+
 	@Scheduled(fixedRate = 1000)
 	public void reportCurrentTime() {
 
 		System.out.println("Sysout Msg" + "The time now is {}" + LocalDateTime.now());
-		
-		LOGGER.info("Logger Msg" +"The time now is {}", LocalDateTime.now());
+
+		logger.info("Logger Msg" +"The time now is {}", LocalDateTime.now());
 
 	}
 }
