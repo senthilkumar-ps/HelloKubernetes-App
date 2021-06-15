31097fc4b89bded307c2eea7935c36ff529306e8
Senthilkumar <psysenthilkumar@gmail.com>
2021-06-15 20:17:57 +0530

    helm chart added

--- src/main/java/com/test/HelloKubernetes.java
+++ src/main/java/com/test/HelloKubernetes.java
@@ -1,13 +1,18 @@
 package com.test;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloKubernetes {
 
+	@Value("${app.message}")
+	private String message;
+
 	@RequestMapping("/")
 	public String index() {
-		return "Hello Kubernetes..!";
+
+		return message;
 	}
 }
